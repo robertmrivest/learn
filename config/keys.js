d0d64d660c39c7c09939ae3e1312943977e460ce
Robert Rivest <robertrivest68@gmail.com>
2019-05-20 09:08:44 -0400

    finish the Auth Flow

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// decide credentials
+
+if (process.env.NODE_ENV === 'production'){
+  module.exports = require('./prod')
+} else {
+ module.exports = require('./dev')
+}
+
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/user');
+require('./services/passport');
+const keys = require('./config/keys');
 
-app.get('/', (req,res)=>{
-res.send({Password: 'No Password'})
-})
+const app = express();
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT)
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+
+//Authentication Routes
+require('./routes/authRoutes')(app);
+
+// Connection and Express Start
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
